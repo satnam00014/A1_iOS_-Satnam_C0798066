bafcafd62ceb6eb973cbb7e16a905775c7849c20
satnam <satnamkahlon4@gmail.com>
2021-05-17 13:02:04 +0530

    adding label to polyline

--- A1_iOS_ Satnam_C0798066/ViewController.swift
+++ A1_iOS_ Satnam_C0798066/ViewController.swift
@@ -7,7 +7,7 @@
 //following are some references to create this application
 //https://developer.apple.com/documentation/corelocation/cllocation/1423689-distance
 //https://stackoverflow.com/questions/47959193/swift-4-to-remove-only-one-annotation-not-all
-
+//https://stackoverflow.com/questions/28643554/where-is-mapkit-step-polyline
 
 
 import UIKit
@@ -27,6 +27,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
 
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var locationButton: UIButton!
+    @IBOutlet weak var zoomButton: UIButton!
     
     //initial value of places
     var places = [Places("A", CLLocationCoordinate2DMake(0, 0)),
@@ -51,6 +52,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         locationButton.layer.cornerRadius = locationButton.bounds.height/2
         locationButton.isHidden = true
         
+        //to round the zoom button
+        zoomButton.layer.cornerRadius = zoomButton.bounds.height/2
+        zoomButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
         //adding map delegate to show polyline,polygon
         //and manymore things on map
         map.delegate = self
@@ -66,10 +71,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         
         //start location update
         locationManager.startUpdatingLocation()
-        
-        // following is used for long press
-        //let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addingLocationsWithGesture))
-        //map.addGestureRecognizer(longPressGesture)
         
         // tap gesture
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(addingLocationsWithGesture))
@@ -140,6 +141,8 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
             let polygon = MKPolygon(coordinates: coordinates, count: coordinates.count)
             polygon.accessibilityLabel = "this is polygon"
             map.addOverlay(polygon)
+            let rect = polygon.boundingMapRect
+            map.setVisibleMapRect(rect, edgePadding: UIEdgeInsets(top:10,left:40,bottom:10,right:40), animated: true)
             
         }else{
             locationButton.isHidden = true
@@ -157,6 +160,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         //adding distance from userlocation to marker in Subtitle
         let userLocation = MKPlacemark(coordinate: self.userLocation!.coordinate)
         let makerLocation = MKPlacemark(coordinate: coordinate)
+        
         //request direction
         let directionRequest = MKDirections.Request()
         directionRequest.source = MKMapItem(placemark: userLocation)
@@ -194,10 +198,16 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         var secondMarker: MKPlacemark
         
         for i in 0...2{
+            var startName: String?
+            var destinationName : String?
             if i == 2 {
+                startName = places[0].title!
+                destinationName = places[i].title!
                 firstMarker = MKPlacemark(coordinate: places[0].coordinate)
                 secondMarker = MKPlacemark(coordinate: places[i].coordinate)
             }else{
+                startName = places[i].title!
+                destinationName = places[i+1].title!
                 firstMarker = MKPlacemark(coordinate: places[i].coordinate)
                 secondMarker = MKPlacemark(coordinate: places[i+1].coordinate)
             }
@@ -218,14 +228,24 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
                 let route = directionResponse.routes[0]
                 //draw polyline
                 self.map.addOverlay(route.polyline, level: .aboveRoads)
-                
                 //define map boundary for screen
                 let rect = route.polyline.boundingMapRect
                 self.map.setVisibleMapRect(rect, edgePadding: UIEdgeInsets(top:100,left:100,bottom:100,right:100), animated: true)
+                
+                // following to adding polyline distance annotation between two points
+                let annotation = MKPointAnnotation()
+                annotation.title = "\(startName!) to \(destinationName!)"
+                annotation.subtitle = "Direction :\(round(route.distance)/1000.0) km"
+                let pointCounts = route.polyline.pointCount
+                let coordinateArray = UnsafeMutablePointer<CLLocationCoordinate2D>.allocate(capacity: pointCounts)
+                route.polyline.getCoordinates(coordinateArray, range: NSMakeRange(0, pointCounts))
+                annotation.coordinate = coordinateArray[pointCounts/2]
+                self.map.addAnnotation(annotation)
+               
+                
             }
         }
     }
-    
     
     //this function is called whenever user location is changed
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -246,12 +266,26 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         map.setRegion(region, animated: true)
         
     }
+   
+    //this is function for enable and disable map zooming
+    @IBAction func enableMapZoom(_ sender: UIButton) {
+        if map.isZoomEnabled {
+            sender.setTitle("Enable zoom", for: .normal)
+            sender.backgroundColor = .systemBlue
+            map.isZoomEnabled = false
+        }else{
+            sender.setTitle("Disable zoom", for: .normal)
+            sender.backgroundColor = .systemGray2
+            map.isZoomEnabled = true
+        }
+    }
+    
     
     //for rendering the polyline and polygon
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         if overlay is MKPolyline{
             let renderer = MKPolylineRenderer(overlay: overlay)
-            renderer.strokeColor = .blue
+            renderer.strokeColor = .systemBlue
             renderer.lineWidth = 5
             return renderer
         } else if overlay is MKPolygon{
@@ -267,6 +301,17 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         if let annotationTitle = view.annotation?.title{
             print("User select \(annotationTitle!)")
+        }
+    }
+    
+    // to change annotation color for polyline label
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.subtitle??.contains("Direction") == true{
+            let annotaionView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "polyline_distance")
+            annotaionView.markerTintColor = UIColor.systemTeal
+            return annotaionView
+        }else {
+            return nil
         }
     }
     
